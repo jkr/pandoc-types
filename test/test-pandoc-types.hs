6940b09b9be1aa47bff11cacd98b9cd7f481fab0
Jesse Rosenthal <jrosenthal@jhu.edu>
2016-09-28 17:15:02 -0400

    Add simple quickcheck tests.
    These just test that roundtrip works properly. When we have a new
    format, we can add HUnit tests against that too.

---
+++ test/test-pandoc-types.hs
@@ -0,0 +1,14 @@
+import Text.Pandoc.Definition
+import Text.Pandoc.Arbitrary
+import Test.QuickCheck
+import Data.Aeson
+
+prop_roundtrip :: Meta -> Bool
+prop_roundtrip doc = case decode $ encode doc :: (Maybe Meta) of
+  Just doc' -> doc == doc'
+  _         -> False
+
+main :: IO ()
+main = quickCheckWith stdArgs { maxSuccess = 500 }  prop_roundtrip
+
+
